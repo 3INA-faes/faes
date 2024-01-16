16d77765d140be0500daa90423e498a21dfe7d9c
3INA-faes <146919404+3INA-faes@users.noreply.github.com>
2024-01-16 16:17:27 +0100

    Add files via upload

---
+++ src/esercizi/Biglietto/BigliettoFerroviarioTest.java
@@ -0,0 +1,27 @@
+package Biglietto;
+
+import java.util.Scanner;
+
+public class BigliettoFerroviarioTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        float tariffa;
+        String condizioneUtente;
+        
+        System.out.println("Inserire la tariffa del biglietto: ");
+        tariffa = in.nextFloat();
+        
+        System.out.println("Se l'utente é pensionato inserire un (P), se è uno "
+                + "studente inserire (S), se é disoccupato inserire (D) e se non"
+                + " riguarda queste caratteristiche inserire (N): ");
+        condizioneUtente = in.next();
+        
+        BigliettoFerroviario b = new BigliettoFerroviario(tariffa,condizioneUtente);
+        
+        System.out.println("Dati di input: " + b.info());
+        
+        System.out.println(b.importo());
+    }
+}
