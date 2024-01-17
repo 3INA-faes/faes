981fcda8fcb9b4afd46d9c3291cfa638ccc70320
3INA-faes <146919404+3INA-faes@users.noreply.github.com>
2024-01-17 09:12:39 +0100

    Add files via upload

---
+++ src/esercizi/laboratorio/Cicli.java
@@ -0,0 +1,47 @@
+package esercizi.laboratorio;
+
+import java.util.Scanner;
+        
+public class Cicli {
+
+    public static void main(String[] args) {
+        // PRECONDIZIONALI
+        Scanner in = new Scanner(System.in);
+        
+        /*
+        String testo;
+        
+        for (int i = 0; i < 10; i++){
+            testo = in.nextLine();
+            System.out.println("Ciao mondo!");
+        }
+        */
+        
+        /*
+        int i = 0;
+        while (i < N){
+            System.out.println(i);
+            i++;
+        }
+        */
+        
+        /*
+        int numero = 1;
+        
+        while (numero != 0){
+            System.out.println("Inserisci il numero: ");
+            numero = in.nextInt();
+        }
+        */
+        
+        
+        
+        // POSTCONDIZIONALE
+        /*
+        do {
+            System.out.println("Inserisci il numero: ");
+            numero = in.nextInt();
+        } while (numero != 0);
+        */
+    }
+}
