201760bf654158ba9b7b0d636217c52a4fafa410
3INA-faes <146919404+3INA-faes@users.noreply.github.com>
2024-02-07 10:17:13 +0100

    Add files via upload

---
+++ src/esercizi/laboratorio/SuperEnalotto.java
@@ -0,0 +1,44 @@
+package esercizi.laboratorio;
+
+/**
+ * Realizzare il gioco del SuperEnalotto semplificato consideriamo pertanto le seguenti ipotesi:
+ * - i numeri univoci da indovinare sono 6, generati casualmente da 1 a 9;
+ * - per vincere, i numeri scelti dall'utente devono essere nella stessa sequenza di quelli generati dalla classe.
+ * @author dennis.faes  3INA 2023
+ * @version 1.0
+ */
+public class SuperEnalotto {
+    public static String gioco(){
+        String numeri;
+        int num1, num2, num3, num4, num5, num6;
+        num1 = (int) (Math.random()*9)+1;
+        num2 = (int) (Math.random()*9)+1;
+        num3 = (int) (Math.random()*9)+1;
+        num4 = (int) (Math.random()*9)+1;
+        num5 = (int) (Math.random()*9)+1;
+        num6 = (int) (Math.random()*9)+1;
+        numeri = num1 + "\t" + num2 + "\t" + num3 + "\t" + num4 + "\t" + num5 + "\t" + num6;
+        return numeri;
+    }
+    
+    
+    public static String gioco1(){
+        int num1, num2, num3, num4, num5, num6;
+        boolean verifica;
+        do{
+        num1 = (int) (Math.random()*9)+1;
+        num2 = (int) (Math.random()*9)+1;
+        num3 = (int) (Math.random()*9)+1;
+        num4 = (int) (Math.random()*9)+1;
+        num5 = (int) (Math.random()*9)+1;
+        num6 = (int) (Math.random()*9)+1;
+        if ((num1 != num2) && (num1 != num3) && (num1 != num4) && (num1 != num5) && (num1 != num6)){
+            if ((num2 != num3) && (num2 != num4) && (num2 != num5) && (num2 != num6)){
+                
+            }
+        }
+            
+            
+        }while ();
+    }
+}
---
+++ src/esercizi/laboratorio/SuperEnalottoTest.java
@@ -0,0 +1,19 @@
+package esercizi.laboratorio;
+
+import java.util.Scanner;
+
+public class SuperEnalottoTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String tentativo;
+        String soluzione;
+        
+        
+        
+        soluzione = SuperEnalotto.gioco();
+        soluzione = SuperEnalotto.gioco1();
+    }
+    
+}
